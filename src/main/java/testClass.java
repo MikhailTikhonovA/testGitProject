2472534c5e7772ccdd5b0152e6bf1fc2061ffd6f
Mikhail Tikhonov <46346390+MikhailTikhonovA@users.noreply.github.com>
2022-01-03 12:38:40 +0300

    test word

--- src/main/java/testClass.java
+++ src/main/java/testClass.java
@@ -1,5 +1,9 @@
+import com.sun.corba.se.impl.orb.ParserTable;
+
 public class testClass {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println(ParserTable.TestAcceptor1.class);
     }
+
 }
